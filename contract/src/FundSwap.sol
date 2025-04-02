5193c905708f15f4d830b8ba07cb521873847c30
genovapink <130900923+genovapink@users.noreply.github.com>
2025-04-02 17:54:43 +0700

    Update FundSwap.sol

--- contract/src/FundSwap.sol
+++ contract/src/FundSwap.sol
@@ -1,78 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./FeeReceiver.sol";
+contract FundSwap {
+    address[] public multisigWallets;
+    mapping(address => bool) public isMultisig;
 
-interface ILiquidityManager {
-    function migrateToUniswap(address token) external;
-}
-
-contract FundSwap is Ownable {
-    address public immutable EDU;
-    FeeReceiver public feeReceiver;
-    ILiquidityManager public liquidityManager;
-
-    uint256 public constant FEE_PERCENT = 150; // 1.5% (basis points: 150 / 10000)
-
-    mapping(address => mapping(address => uint256)) public liquidity;
-    mapping(address => mapping(address => uint256)) public balances;
-
-    event Swap(address indexed user, address indexed token, uint256 amountEDU, uint256 amountToken);
-    event AddLiquidity(address indexed provider, address indexed token, uint256 amountEDU, uint256 amountToken);
-    event RemoveLiquidity(address indexed provider, address indexed token, uint256 amountEDU, uint256 amountToken);
-
-    constructor(address _EDU, address _feeReceiver, address _liquidityManager) {
-        EDU = _EDU;
-        feeReceiver = FeeReceiver(_feeReceiver);
-        liquidityManager = ILiquidityManager(_liquidityManager);
+    modifier onlyMultisig() {
+        require(isMultisig[msg.sender], "Not authorized");
+        _;
     }
 
-    function swapEDUForToken(address token, uint256 amountEDU) external {
-        require(IERC20(EDU).transferFrom(msg.sender, address(this), amountEDU), "Transfer failed");
-
-        uint256 fee = (amountEDU * FEE_PERCENT) / 10000;
-        uint256 amountAfterFee = amountEDU - fee;
-
-        feeReceiver.collectFee(EDU, fee);
-
-        uint256 tokenAmount = getTokenAmount(token, amountAfterFee);
-        require(IERC20(token).transfer(msg.sender, tokenAmount), "Token transfer failed");
-
-        emit Swap(msg.sender, token, amountEDU, tokenAmount);
+    constructor() {
+        multisigWallets.push(0xbfFf72a006B8A41abF693B7d6db28bd8F1b0a074); // Wallet pertama
+        multisigWallets.push(0xE90A9B7c620923e68FE5C3aB5Ee427f57C64b427); // Wallet kedua
+        
+        isMultisig[0xbfFf72a006B8A41abF693B7d6db28bd8F1b0a074] = true; // Wallet pertama
+        isMultisig[0xE90A9B7c620923e68FE5C3aB5Ee427f57C64b427] = true; // Wallet kedua
     }
 
-    function addLiquidity(address token, uint256 amountEDU, uint256 amountToken) external {
-        require(IERC20(EDU).transferFrom(msg.sender, address(this), amountEDU), "Transfer failed");
-        require(IERC20(token).transferFrom(msg.sender, address(this), amountToken), "Transfer failed");
-
-        liquidity[msg.sender][token] += amountEDU;
-        balances[msg.sender][token] += amountToken;
-
-        emit AddLiquidity(msg.sender, token, amountEDU, amountToken);
+    function addMultisigWallet(address _wallet) external onlyMultisig {
+        require(!isMultisig[_wallet], "Already a multisig");
+        multisigWallets.push(_wallet);
+        isMultisig[_wallet] = true;
     }
 
-    function removeLiquidity(address token, uint256 amountEDU, uint256 amountToken) external {
-        require(liquidity[msg.sender][token] >= amountEDU, "Not enough liquidity");
-        require(balances[msg.sender][token] >= amountToken, "Not enough token balance");
-
-        liquidity[msg.sender][token] -= amountEDU;
-        balances[msg.sender][token] -= amountToken;
-
-        require(IERC20(EDU).transfer(msg.sender, amountEDU), "EDU transfer failed");
-        require(IERC20(token).transfer(msg.sender, amountToken), "Token transfer failed");
-
-        emit RemoveLiquidity(msg.sender, token, amountEDU, amountToken);
-    }
-
-    function migrateToUniswap(address token) external onlyOwner {
-        liquidityManager.migrateToUniswap(token);
-    }
-
-    function getTokenAmount(address token, uint256 amountEDU) public view returns (uint256) {
-        uint256 tokenReserve = IERC20(token).balanceOf(address(this));
-        uint256 eduReserve = IERC20(EDU).balanceOf(address(this));
-        return (amountEDU * tokenReserve) / eduReserve;
+    function removeMultisigWallet(address _wallet) external onlyMultisig {
+        require(isMultisig[_wallet], "Not a multisig");
+        isMultisig[_wallet] = false;
+        for (uint i = 0; i < multisigWallets.length; i++) {
+            if (multisigWallets[i] == _wallet) {
+                multisigWallets[i] = multisigWallets[multisigWallets.length - 1];
+                multisigWallets.pop();
+                break;
+            }
+        }
     }
 }
