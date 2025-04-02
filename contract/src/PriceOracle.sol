6632bac306902bd6e9d672e19409343be1570674
genovapink <130900923+genovapink@users.noreply.github.com>
2025-04-02 17:57:03 +0700

    Update PriceOracle.sol

--- contract/src/PriceOracle.sol
+++ contract/src/PriceOracle.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 contract PriceOracle {
-    function getPrice() external pure returns (uint256) {
+    function getPrice () external pure returns (uint256) {
         return 100;
     }
 }
