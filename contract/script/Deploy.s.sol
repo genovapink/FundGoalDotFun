592d0230f5ce512f70175be1f952ff2165b4d940
reiyanyan <hariyantoriyan.hr@gmail.com>
2025-04-04 16:59:19 +0700

    feat: makefile for deploy contract

--- contract/script/Deploy.s.sol
+++ contract/script/Deploy.s.sol
@@ -1,3 +1,32 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { TokenLauncher } from "../src/TokenLauncher.sol";
+
+// import { FeeReceiver } from "../src/FeeReceiver.sol";
+
+contract DeployAll is Script {
+  function run() external {
+    vm.startBroadcast();
+
+    TokenLauncher tokenLauncher = new TokenLauncher(
+      "Test Rei",
+      "REI",
+      address(this),
+      address(0x1234567890123456789012345678901234567890), // Replace with actual fee receiver address
+      address(0x1234567890123456789012345678901234567890), // Replace with actual price oracle address
+      address(0x1234567890123456789012345678901234567890), // Replace with actual vesting contract address
+      1000
+    );
+    console.log("TokenLauncher deployed at:", address(tokenLauncher));
+
+    /* ---------------------- Soon contracts to be deployed --------------------- */
+    // FeeReceiver feeReceiver = new FeeReceiver();
+    // console.log("FeeReceiver deployed at:", address(feeReceiver));
+
+    vm.stopBroadcast();
+  }
+}
