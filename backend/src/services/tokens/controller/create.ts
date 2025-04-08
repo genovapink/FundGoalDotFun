e174f7028dab0a43b1c5f4f14dc44c79362187e6
reiyanyan <hariyantoriyan.hr@gmail.com>
2025-04-08 07:08:56 +0700

    feat: ipfs

--- backend/src/services/tokens/controller/create.ts
+++ backend/src/services/tokens/controller/create.ts
@@ -1,13 +1,31 @@
-import { Request, Response } from "express";
 import { TokenModel } from "../model";
+import { pinata } from "@packages/pinata";
+import { Request, Response } from "express";
 
 export const createItem = async (req: Request, res: Response) => {
-    try {
-        const newItem = new TokenModel(req.body);
-        await newItem.save();
-        res.status(201).json(newItem);
-    } catch (error) {
-        console.error("Error creating item:", error);
-        res.status(500).json({ message: "Failed to create item." });
-    }
+  try {
+    const file = req.file;
+    const jsonData = req.body.data;
+
+    if (!file) res.status(400).json({ error: "Image file missing" });
+    if (!jsonData) res.status(400).json({ error: "Data field missing" });
+
+    // parsedData = JSON.parse(jsonData);
+    const pinataFile = new File([file!.buffer], file!.originalname, {
+      type: file!.mimetype,
+    });
+    const upload = await pinata.upload.public.file(pinataFile);
+
+    /* --------------------- this one should be saved to db --------------------- */
+    const urlPinata = await pinata.gateways.public.convert(upload.cid);
+
+    res.status(200).json({ message: "Received", upload, urlPinata, data: jsonData });
+
+    // const newItem = new TokenModel(req.body);
+    // await newItem.save();
+    // res.status(201).json(newItem);
+  } catch (error) {
+    console.error("Error creating item:", error);
+    res.status(500).json({ message: "Failed to create item." });
+  }
 };
--- backend/src/services/tokens/routes/index.ts
+++ backend/src/services/tokens/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { findItems } from "../controller/find";
 import { createItem } from "../controller/create";
+import multer from "multer";
 
 const router = Router();
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 router.get("/", findItems);
-router.post("/", createItem);
+router.post("/", upload.single("image"), createItem);
 
 export { router };
